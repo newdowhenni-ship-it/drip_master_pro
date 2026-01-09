d8f4ffd08b890cbb2c6aaecf80c2b6e2f8517244
newdowhenni-ship-it <newdow.henni@gmail.com>
2026-01-10 02:06:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
 const CACHE_NAME = 'drip-master-v1';
-const urlsToCache = [
+const ASSETS = [
   '/drip_master_pro/',
   '/drip_master_pro/index.html',
   '/drip_master_pro/manifest.json'
 ];
 
-// インストール時にファイルをキャッシュする
+// インストール時にリソースをキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// オフライン時でもキャッシュから読み込む
+// キャッシュがあればそれを返し、なければネットワークから取得
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
